ed4a9aa339a3aabf9888b94cab5b00b85b797e25
XXMLP <1836942145@qq.com>
2020-03-07 18:14:22 +0800

    添加镜像文件，shell脚本

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+#注意，docker命令全部大写，这是规定。
+#   From 关键字表示，jar包依赖的环境。java:8  相当于jdk1.8
+FROM java:8
+
+#ADD命令
+#   blog-0.0.1-SNAPSHOT.jar：这是你上传jar包的名称。
+#   /blog.jar：这是自定义的名称。但是注意要有之前的/
+ADD blog-0.0.1-SNAPSHOT.jar /blog.jar
+#COPY jacocoagent.jar $APP_DIR
+
+#MAINTAINER  作者名称。可以删除不写。
+MAINTAINER xxmlp
+
+#EXPOSE 项目暴露的端口号
+EXPOSE 8080
+
+#/blog.jar此处的名称要和ADD命令后面的一样。
+ENTRYPOINT ["java","-jar","/blog.jar"]
---
+++ deploy.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+
+DOCKER_DIR=/root/docker
+mkdir -p $DOCKER_DIR
+cd $DOCKER_DIR
+
+#如果存在旧的blog文档，则删除
+if [ -d "blog" ];
+then
+    echo "rm dir blog"
+	rm -rf blog
+	sleep 10
+	echo "rm over"
+else
+    echo "dir blog not exit"
+fi
+
+#下载源码
+git clone git@github.com:XXMLP/blog2.git
+
+echo "git clone over"
+#打包
+cd $DOCKER_DIR/blog
+mvn package
+
+sleep 10
+echo "package over"
+
+cd $DOCKER_DIR
+#删除原有的文件
+rm -f Dockerfile
+rm -f blog-0.0.1-SNAPSHOT.jar
+
+mv $DOCKER_DIR/blog/target/blog-0.0.1-SNAPSHOT.jar .
+mv $DOCKER_DIR/blog/Dockerfile .
+
+#删除旧的镜像和容器
+docker stop blog
+docker rm blog
+docker rmi blog:1.0
+sleep 10
+#打包成镜像
+#说明:
+#  blog   代表要打包成的镜像名称。按照自己实际情况写。
+#  :1.0   代表版本号，可以不写则默认为latest
+#  .    代表为当前目录。这就是为什么一直在步骤一文件夹中进行操作,并且Dockerfile在此文件夹中的原因。
+#
+#若之前Dockerfile不在步骤一的文件夹中 则需要指定到对应的地址
+docker build -t blog:1.0 .
+#启动容器，前提是已开放这个端口
+docker run --name blog -d -p 80:8080 blog:1.0
+
+#mvn clean verify -f pom_cc.xml
+#jar -xvf blog-0.0.1-SNAPSHOT.jar
+#java -javaagent:jacocoagent.jar=includes=*,output=tcpserver,port=2015,address=localhost -jar blog-0.0.1-SNAPSHOT.jar
+#mvn clean verify -f pom_onthefly.xml
