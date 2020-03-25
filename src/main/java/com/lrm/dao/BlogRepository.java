8e474e6b0debb83081b581eec4e6dd93cc8c7755
XXMLP <1836942145@qq.com>
2020-03-25 17:14:05 +0800

    修复了首页存在未发布博客的bug

--- src/main/java/com/lrm/dao/BlogRepository.java
+++ src/main/java/com/lrm/dao/BlogRepository.java
@@ -15,9 +15,12 @@ public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificat
 
     @Query("select b from Blog b where b.recommend = true")
     List<Blog> findTop(Pageable pageable);
-    
+
     @Query("select b from Blog b where b.published = true")
     List<Blog> findNewAll(Pageable pageable);
+
+    @Query("select b from Blog b where b.published = true")
+    Page<Blog> findAll(Pageable pageable);
 
     @Query("select b from Blog b where b.title like ?1 or b.content like ?1")
     Page<Blog> findByQuery(String query,Pageable pageable);
