72d8c8fc93d776d54dfa6cfdec7fa5f265ba56a7
lipeng.min <1836942145@qq.com>
2020-04-30 15:41:25 +0800

    添加博客点赞功能

---
+++ src/main/java/com/xxmlp/dao/ThumbsRepository.java
@@ -0,0 +1,16 @@
+package com.xxmlp.dao;
+
+import com.xxmlp.po.Thumbs;
+import com.xxmlp.po.ThumbsPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ThumbsRepository extends JpaRepository<Thumbs, ThumbsPK> {
+
+    @Query("select u from Thumbs u where u.blogId = ?1 and u.userId = ?2")
+    Thumbs isThumbed(Long blogId, Long userId);
+
+
+    Integer countByBlogId(Long blogId);
+
+}
