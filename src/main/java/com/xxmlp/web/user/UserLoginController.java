f3fb16a67f0aaa3a38811bd8ea8df5b1257e2bf1
lipeng.min <1836942145@qq.com>
2020-05-15 22:42:21 +0800

    修改真实IP的工具类

--- src/main/java/com/xxmlp/web/user/UserLoginController.java
+++ src/main/java/com/xxmlp/web/user/UserLoginController.java
@@ -5,9 +5,8 @@ import com.xxmlp.po.User;
 import com.xxmlp.service.AdressService;
 import com.xxmlp.service.UserService;
 import com.xxmlp.util.AddrUtil;
-import com.xxmlp.util.IpUtil;
+import com.xxmlp.util.IPUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -42,8 +41,8 @@ public class UserLoginController {
         if (user != null) {
             //user.setPassword(null);
             session.setAttribute("user",user);
-            address.setIp(IpUtil.getIpAddr(request));
-            address.setAddress(AddrUtil.getURLContent(IpUtil.getIpAddr(request)));
+            address.setIp(IPUtil.getIpAddr(request));
+            address.setAddress(AddrUtil.getURLContent(IPUtil.getIpAddr(request)));
             address.setUser(user);
             adressService.save(address);
             return "user/index";
